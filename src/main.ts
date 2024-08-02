73753b30b23ad167a495749b83075942bef2e613
V1SHAL421 <vishalmkumar3@gmail.com>
2024-08-02 20:05:17 +0100

    fix: Reformat features folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/features/main-page/app.config';
-import { AppComponent } from './app/features/main-page/app.component';
+// import { appConfig } from './app/features/main-page/app.config';
+import { appConfig } from './app/features/app.config';
+import { AppComponent } from './app/features/core/main-page/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
