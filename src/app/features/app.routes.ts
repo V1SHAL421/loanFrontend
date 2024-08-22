7f1040737ffed53ce26a51b1bf7411bcbcca130a
V1SHAL421 <vishalmkumar3@gmail.com>
2024-08-22 21:37:10 +0100

    feat: Implement route based lazy loading

--- src/app/features/app.routes.ts
+++ src/app/features/app.routes.ts
@@ -8,10 +8,10 @@ import { AuthGuard } from '../core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'loan', component: LoanComponent, canActivate: [AuthGuard] },
-    { path: 'signup', component: SignupComponent},
-    { path: 'user-details', component: UserDetailsComponent, canActivate: [AuthGuard]}
+    { path: 'home', loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent) },
+    { path: 'login', loadComponent: () => import('./login/login.component').then((c) => c.LoginComponent)},
+    { path: 'loan', loadComponent: () => import('./loan/loan.component').then((c) => c.LoanComponent), canActivate: [AuthGuard] },
+    { path: 'signup', loadComponent: () => import('./signup/signup.component').then((c) => c.SignupComponent)},
+    { path: 'user-details', loadComponent: () => import('./user-details/user-details.component').then((c) => c.UserDetailsComponent), canActivate: [AuthGuard]}
     // { path: '**', component: NotFoundComponent }
 ];
